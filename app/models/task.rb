04fe95f3400e279b2a5ca669449f4e78b1d41b48
EC2 Default User <ec2-user@ip-172-31-44-121.ap-northeast-1.compute.internal>
2026-02-13 02:56:08 +0000

    feat: priorityを追加しタスクの優先度管理機能を実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,10 @@ class Task < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  enum status: {not_started: 0, in_progress:1, done: 2}
+  enum priority: {low: 0, medium: 1, high: 2}
+
   validates :title, presence: true
+  validates :body, presence: true
   
 end
--- db/migrate/20260201102357_create_tasks.rb
+++ db/migrate/20260201102357_create_tasks.rb
@@ -2,7 +2,10 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :completed
+      t.text :body
+      t.integer :status, null: false, default: 0
+      t.integer :priority, null: false, default: 0
+      t.datetime :due_date
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
