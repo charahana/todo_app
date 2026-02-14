8eb75e899805483789f7e6201a664523ccd1c811
EC2 Default User <ec2-user@ip-172-31-44-121.ap-northeast-1.compute.internal>
2026-02-14 07:02:45 +0000

    feat: コントローラのindexを定義

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,18 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    tasks = current_user.tasks
+    @total_tasks = tasks.tasks_count
+    @completd_tasks = tasks.done.count
+    @in_progress_tasks = tasks.in_progress.count
+    @not_started_tasks = tasks.not_started.count
+    @completion_rate =
+      if @total_tasks.zero?
+        0
+      else
+        (@completed_tasks.to_f / @total_tasks * 100).round
+      end
+    end
   end
 end
