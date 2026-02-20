5dc65961615341beb034292d8a9e7d9e28506bed
EC2 Default User <ec2-user@ip-172-31-44-121.ap-northeast-1.compute.internal>
2026-02-20 05:53:08 +0000

    refactor: ファイル内のコード整理

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,27 +4,22 @@ RSpec.describe "Tasks", type: :system do
   let(:user) { create(:user) }
 
   before do
-    driven_by(:rack_test) # 軽量ブラウザ
+    driven_by(:rack_test)
   end
 
   it "ユーザーがタスクを作成し削除できる" do
-    # ログイン
     sign_in user
 
-    # 新規作成画面へ
     visit new_task_path
 
-    # フォーム入力
     fill_in "タイトル", with: "テストタスク"
     fill_in "内容", with: "システムスペック確認"
 
     click_button "作成"
 
-    # 作成成功メッセージ確認
     expect(page).to have_content "タスクを作成しました"
     expect(page).to have_content "テストタスク"
 
-    # 削除
     click_link "削除"
 
     expect(page).to have_content "タスクを削除しました"
