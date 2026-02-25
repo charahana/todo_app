fe44da57e5dfe93da1583271c93d9cd5626f53a3
charahana <charatwoha@outlook.jp>
2026-02-25 10:13:43 +0000

    test: TODOリストのフィルタ及び絞り込みテストを実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ end
 
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless') # GUIなしで実行
+  options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
   options.add_argument('--window-size=1400,1400')
