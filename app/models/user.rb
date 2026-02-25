f9613996f9bfb7b45bc8235995aa54835db8a981
charahana <charatwoha@outlook.jp>
2026-02-25 10:13:44 +0000

    refactor: has_manyタイプミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :tasks, dependent: :destroy
-  has_mary :memberships, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
   has_one_attached :profile_image
 
