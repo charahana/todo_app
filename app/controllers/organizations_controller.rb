5b765aa8420412bda95675983c71be22bdc1e289
charahana <charatwoha@outlook.jp>
2026-02-26 05:45:18 +0000

    feat: ユーザー管理機能を追加

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,11 @@
 class OrganizationsController < ApplicationController
+  def create
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      @organization.memberships.create(user: current_user, role: :admin)
+      redirect_to @organization, notice: "作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
