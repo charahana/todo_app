c61c9162bd7ec716c8478530c44d0d2e42fb4ceb
charahana <charatwoha@outlook.jp>
2026-02-27 13:22:46 +0000

    feat: ヘッダーにメンバー管理へのリンクを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def current_organization
+    return nil unless user_signed_in?
+    @current_organization ||= current_user.organizations.first
+  end
+  helper_method :current_organization
 end
