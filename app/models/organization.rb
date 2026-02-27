ae726ea8bb884ccecfa93dda7ceba100f85eaf0d
charahana <charatwoha@outlook.jp>
2026-02-27 08:42:53 +0000

    feat: 管理者判定メソッドを追加

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,8 @@
 class Organization < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
+
+  def admin?(user)
+    memberships.find_by(user: user)&.admin?
+  end
 end
