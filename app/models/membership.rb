24968f04d1c2d995b0497139cb807915e415be0c
charahana <charatwoha@outlook.jp>
2026-02-28 07:53:10 +0000

    feat: 最後の管理者を削除・降格出来ない制御を追加

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,4 +3,18 @@ class Membership < ApplicationRecord
   belongs_to :organization
 
   enum role: { member: 0, admin: 1 }
+
+  validate :cannot_remove_last_admin, if: :will_be_non_admin?
+
+  private
+
+  def will_be_non_admin?
+    admin? && (role_changed? || destroyed?)
+  end
+
+  def cannot_remove_last_admin
+    if organization.memberships.admin.count == 1
+      errors.add(:base, "最後の管理者は削除または変更出来ません")
+    end
+  end
 end
