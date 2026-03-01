e6bd5381fab20938af0e8d050ee8b0468c6b0a59
charahana <charatwoha@outlook.jp>
2026-03-01 05:57:06 +0000

    feat: ユーザー名のバリデーションを実装

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,8 @@ class Organization < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
+  validates :name, presence: true
+
   def admin?(user)
     memberships.find_by(user: user)&.admin?
   end
