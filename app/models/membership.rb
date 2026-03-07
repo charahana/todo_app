c02126f5ecda67926d9326952408efe532c29316
charahana <charatwoha@outlook.jp>
2026-03-07 10:23:47 +0000

    refactor: memberモデル内のバリデーションとコールバックの重複を削除

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,21 +4,20 @@ class Membership < ApplicationRecord
 
   enum role: { member: 0, admin: 1 }
 
-  validate :cannot_demote_last_admin, if: :will_save_change_to_role?
-  before_destroy :cannot_destroy_last_admin
-  before_update :cannot_demote_last_admin
+  before_update :prevent_last_admin_demotion
+  before_destroy :prevent_last_admin_destroy
 
   private
 
-  def cannot_destroy_last_admin
-    if admin? && organization.memberships.admin.count == 1
+  def prevent_last_admin_demotion
+    if role_changed? && role_was == "admin" && organization.memberships.admin.count == 1
       errors.add(:base, "最後の管理者は削除または変更できません")
       throw :abort
     end
   end
 
-  def cannot_demote_last_admin
-    if role_changed? && role_was == "admin" && role != "admin" && organization.memberships.admin.count == 1
+  def prevent_last_admin_destroy
+    if admin? && organization.memberships.admin.count == 1
       errors.add(:base, "最後の管理者は削除または変更できません")
       throw :abort
     end
