e389b0ef5ae2b2f61a0c1279343c551503288a28
charahana <charatwoha@outlook.jp>
2026-03-10 14:23:01 +0000

    feat: ダッシュボードのhelperを追加

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,17 @@
 module DashboardHelper
+    def completion_bar_color(rate)
+      case rate
+      when 0..29
+        "bg-danger"
+      when 30..69
+        "bg-info"
+      else
+        "bg-success"
+      end
+    end
+    
+    def formatted_date(date)
+      return "" if date.blank?
+      date.strftime("%m/%d")
+    end
 end
